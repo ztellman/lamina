48defb1f5caefe86649852cd7789b7dbefcfeb8c
prospero <ztellman@gmail.com>
2010-11-02 20:28:58 -0700

    first commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject lamina "0.3.0-SNAPSHOT"
+  :description "event-driven data structures for clojure"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [potemkin "0.1.0"]]
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo})
