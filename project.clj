729857a0478502ed41ec6e9e1c7a63dad3248b37
ztellman <ztellman@gmail.com>
2010-11-06 18:16:05 -0700

    retroactively put namespace change in 0.3.1-snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lamina "0.3.0-SNAPSHOT"
+(defproject lamina "0.3.1-SNAPSHOT"
   :description "event-driven data structures for clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
