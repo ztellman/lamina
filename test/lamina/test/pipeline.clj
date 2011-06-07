7f47241d3049a6a82c107a83345303c08d519ed4
Daniel E. Renfer <duck@kronkltd.net>
2011-06-07 08:56:51 -0400

    using ranges in tests cause tests to fail in clojure 1.3

--- test/lamina/test/pipeline.clj
+++ test/lamina/test/pipeline.clj
@@ -56,8 +56,8 @@
 ;;;
 
 (deftest test-basic-pipelines
-  (test-pipeline (apply pipeline (take 1e3 (repeat inc))) 1e3)
-  (test-pipeline (apply pipeline (take 1e3 (repeat (fn [x] (task (inc x)))))) 1e3)
+  (test-pipeline (apply pipeline (take 1e3 (repeat inc))) 1000)
+  (test-pipeline (apply pipeline (take 1e3 (repeat (fn [x] (task (inc x)))))) 1000)
   (test-pipeline (apply pipeline (take 100 (repeat slow-inc))) 100)
   (test-pipeline (pipeline #(assoc {} :result %) :result) 0))
 
