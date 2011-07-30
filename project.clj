5e43207749734f9f4978d196cf705cf24354e223
ztellman <ztellman@gmail.com>
2011-07-30 11:54:05 -0700

    update to beta1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lamina "0.4.0-alpha3-SNAPSHOT"
+(defproject lamina "0.4.0-beta1"
   :description "event-driven data structures for clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
