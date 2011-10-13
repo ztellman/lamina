4eb0bb70c0a32fe65df1c9b07025522361232bda
Daniel E. Renfer <duck@kronkltd.net>
2011-10-13 16:11:03 -0400

    remove monlithic contrib

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject lamina "0.4.0-beta3-SNAPSHOT"
   :description "event-driven data structures for clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/algo.generic "0.1.0-SNAPSHOT"]
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/math.combinatorics "0.0.2-SNAPSHOT"]
@@ -9,6 +8,8 @@
                  [potemkin "0.1.0"]]
   ;;:jvm-opts ["-agentlib:jdwp=transport=dt_socket,address=8030,server=y,suspend=n"]	
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+  :exclusions [org.clojure/contrib
+               org.clojure/clojure-contrib]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo})
