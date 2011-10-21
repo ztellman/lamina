590bc273e017bd92f2f075bf33aed625587afab3
ztellman <ztellman@gmail.com>
2011-10-21 14:26:20 -0700

    update version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject lamina "0.4.0-rc1"
+(defproject lamina "0.4.1-SNAPSHOT"
   :description "event-driven data structures for clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/algo.generic "0.1.0-SNAPSHOT"]
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/math.combinatorics "0.0.2-SNAPSHOT"]
-                 [org.clojure/tools.logging "0.1.2"]
+                 [org.clojure/tools.logging "0.2.3"]
                  [potemkin "0.1.0"]]
   ;;:jvm-opts ["-agentlib:jdwp=transport=dt_socket,address=8030,server=y,suspend=n"]	
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
