f1212410700dff1957a9339615c239a148a98b72
ztellman <ztellman@gmail.com>
2011-12-02 00:15:07 -0800

    update version for algo.generic

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject lamina "0.4.1-SNAPSHOT"
   :description "event-driven data structures for clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/algo.generic "0.1.0-SNAPSHOT"]
+                 [org.clojure/algo.generic "0.1.0"]
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/math.combinatorics "0.0.2"]
                  [org.clojure/tools.logging "0.2.3"]
