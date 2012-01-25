a47b1a1181f2d40e1b6067f6d18331a9daf33903
Zach Tellman <ztellman@gmail.com>
2012-01-24 22:35:53 -0800

    bump to 0.4.1-alpha1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lamina "0.4.1-SNAPSHOT"
+(defproject lamina "0.4.1-alpha1"
   :description "event-driven data structures for clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
