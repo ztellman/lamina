9cff0fbe2b382e600fd161abe856cc645df3e360
Zach Tellman <ztellman@gmail.com>
2012-04-28 12:51:11 +0530

    mark rc1

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject lamina "0.4.1-beta2"
+(defproject lamina "0.4.1-rc1"
   :description "event-driven data structures for clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [potemkin "0.1.2"]]
+                 [potemkin "0.1.3"]]
   :multi-deps {:all [[org.clojure/tools.logging "0.2.3"]
-                     [potemkin "0.1.1"]]
+                     [potemkin "0.1.3"]]
                "1.2" [[org.clojure/clojure "1.2.1"]]}
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :exclusions [org.clojure/contrib
