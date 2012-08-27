784864bb39a5959bc2e227f2d02e3ab41731e9cf
Zach Tellman <ztellman@gmail.com>
2012-08-27 08:42:48 -0700

    don't test with 1.5.0-master for now

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                      [org.clojure/tools.logging "0.2.3"]
                      [useful "0.8.2"]
                      [potemkin "0.1.5"]]
-               "master" [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]
+               ;;"master" [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]
                "1.2" [[org.clojure/clojure "1.2.1"]]
                "1.4" [[org.clojure/clojure "1.4.0"]]
                }
