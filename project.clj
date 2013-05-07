be6a410fd6b22764347a2528df202c0f155c0b6b
Zach Tellman <ztellman@gmail.com>
2013-05-07 14:38:27 -0700

    use latest criterium

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :exclusions [org.clojure/contrib
                org.clojure/clojure-contrib]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  [criterium "0.3.1"]
+                                  [criterium "0.4.1"]
                                   [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
